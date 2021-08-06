7054254f5208013ca05d889e419e8a30fa52084e
dayle998 <dayle998@naver.com>
2021-08-06 18:25:33 +0900

    feat: 상품 수정

--- src/main/java/simpleSpringMVC/itemservice/web/BasicItemController.java
+++ src/main/java/simpleSpringMVC/itemservice/web/BasicItemController.java
@@ -4,9 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import simpleSpringMVC.itemservice.domain.item.Item;
 import simpleSpringMVC.itemservice.domain.repo.ItemRepo;
 
@@ -32,6 +30,48 @@ public class BasicItemController {
         Item item = itemRepo.findById(itemId);
         model.addAttribute( "item", item);
         return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "/basic/addForm";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepo.save(item);
+
+        model.addAttribute("item", item);
+
+        return "/basic/item";
+    }
+
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepo.save(item);
+        return "/basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        // Item -> item
+        itemRepo.save(item);
+        return "/basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepo.save(item);
+        return "/basic/item";
     }
 
     /**
